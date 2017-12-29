9896f39a11b1d3b77b8d811b8ccfb09018dff367
Donald <donald@shoppingminds.com>
2017-12-29 13:29:33 +0100

    Test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,40 @@ describe('GET /todos/:id', () => {
 			.end(done);
 	});
 });
+
+describe('DELETE /todos/:id', () => {
+	it('should delete todo and return doc', (done) => {
+		var hexId = todos[0]._id.toHexString();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.findById(hexId).then((todos) => {
+					expect(todos).toNotExist();
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		request(app)
+			.delete(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 on invalid id', (done) => {
+		request(app)
+			.delete('/todos/123456')
+			.expect(404)
+			.end(done);
+	});
+});
