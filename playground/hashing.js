16cbdbbf7601a919a7c666faba8ca809820a9f9c
Donald <donald@shoppingminds.com>
2018-01-03 14:34:44 +0100

    Hash password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = 'qwertyuiop';
+
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+var hashedPw = '$2a$10$.U6CsbzCtV6MugVFQQCtbu6FoMm0koqsVAX8EOxNB6nxN0eLISHaO';
+
+bcrypt.compare(password, hashedPw, (err, res) => {
+	console.log(res);
+});
 
 //console.log(SHA256('this is a message').toString());
 
-var data = {
-	id: 10
-};
-
-var token = jwt.sign(data, 'asdfghj');
-
-console.log(token);
-
-var decoded = jwt.verify(token, 'asdfghj');
-
-console.log(decoded);
+// var data = {
+// 	id: 10
+// };
+//
+// var token = jwt.sign(data, 'asdfghj');
+//
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'asdfghj');
+//
+// console.log(decoded);
